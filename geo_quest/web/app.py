e6d8b390a87f019072cfcfe1aedbf66243d79722
Daniel Cocos <dev.danielcocos@gmail.com>
2025-06-10 14:51:53 +0100

    Pages Setup

--- geo_quest/web/app.py
+++ geo_quest/web/app.py
@@ -3,10 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+
+@app.route("/filters.html")
+def settings() -> str:
+    return render_template("filters.html")
+
+@app.route("/index.html")
 def index() -> str:
     return render_template("index.html")
 
+@app.route("/rules.html")
+def rules() -> str:
+    return render_template("rules.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
