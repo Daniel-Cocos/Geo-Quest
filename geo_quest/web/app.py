53fb0fb99597b9bec55f17c0305e48a4765f23f7
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-06-10 20:16:59 +0000

    Add route for /

--- geo_quest/web/app.py
+++ geo_quest/web/app.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
+from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def main() -> Response:
+    return redirect("http://localhost:5000/index.html")
 
 
 @app.route("/filters.html")
 def settings() -> str:
     return render_template("filters.html")
 
+
 @app.route("/index.html")
 def index() -> str:
     return render_template("index.html")
 
+
 @app.route("/rules.html")
 def rules() -> str:
     return render_template("rules.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
